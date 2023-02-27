ac3676f2ee7cdc694b3a0fa2f00548fbd5fa5e40
Krisha2000 <krishasompura20@gmail.com>
2023-02-27 08:15:52 +0530

    krisha here

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route("/n")
+def details():
+    a="Company Name: ABC Corporation"
+    b="Location: India"
+    c="Contact Detail: 999-999-9999"
+    return a,b,c
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+   app.run(host="0.0.0.0")
